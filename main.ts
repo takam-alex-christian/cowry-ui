ce94d6eb91e2d48ca84c25fd95efddd6bb81ee92
takam-alex-christian <takamalexchristian@gmail.com>
2025-08-07 22:25:58 +0800

    project restructuring

---
+++ main.ts
@@ -0,0 +1,4 @@
+import Button from "./src/Universal/Button/index.vue"
+//export all buttons from here
+
+export { Button }
---
+++ src/Universal/Button/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+<script setup>
+</script>
+<style scoped></style>
